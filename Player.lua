09d7db5c8d8b4e9b0e4a3066df1ce045b653ba4d
hannahrajan <66148049+hannahrajan@users.noreply.github.com>
2020-12-31 15:31:29 -0500

    Update Player.lua

--- Player.lua
+++ Player.lua
@@ -49,8 +49,17 @@ function Player:init(map)
         if collider_1.collision_class == 'Player' and collider_2.collision_class == 'Border' then
             contact:setEnabled(false)
             if collider_1:enter(collider_2.collision_class) and self.state ~= 'jumping' then
-                self.lives =  0
-                self.sounds['hit']:play()
+                self.lives = self.lives - 1
+                self.m = 1
+                self.power = 'D'
+                self.x = map.tileW * 10
+                self.y = map.tileH * (map.mapH / 2 - 1) - self.h
+                for i = 1, 4 do
+                    self.map.obs['o' .. i]:reset()
+                    self.map.obs['o' .. i].y = -100
+                    self.map.obs['o' .. i].d = 'down'
+                    self.map.obs['o' .. i].rounds = 0
+                end
                 --[[self.fillerx, self.fillery = collider_2:getPosition()
                 x2, y2 = self.fillerx, self.fillery
                 if self.x >= 210 then--right
